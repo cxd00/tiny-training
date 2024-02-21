09f8e9863b34e4aa9174a444cb5f561f06ac92a9
Cynthia <xcynthia@live.unc.edu>
2024-02-21 19:45:34 +0000

    adding cifar10c dataset

--- algorithm/core/dataset/dataset_entry.py
+++ algorithm/core/dataset/dataset_entry.py
@@ -2,9 +2,49 @@ from .vision import *
 from ..utils.config import configs
 from .vision.transform import *
 import torchvision
+import tensorflow_datasets as tfds
+import numpy as np
+import tensorflow as tf
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 
 __all__ = ['build_dataset']
 
+class CIFAR10CorruptDataset(Dataset):
+    """Corrupted CIFAR10C dataset."""
+
+    def __init__(self, corruption_category_name, split=['test'], transform=None):
+        """
+        Arguments:
+            corruption_category_name (string): tfds name for the corruption to be downloaded.
+            split (array of strings): dictates whether this is a train or test split
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        if split == "train":
+            real_split = ["test[:60%]"]
+        elif split == "val":
+            real_split = ["test[60%:]"]
+        else:
+            real_split = split
+
+        self.ds = list(tfds.load(configs.data_provider.dataset, split=real_split, shuffle_files=True)[0])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.ds)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            raise NotImplementedError
+
+        item = self.ds[idx]
+        img = Image.fromarray(np.array(item["image"]))
+
+        if self.transform:
+            img = self.transform(img)
+
+        return [img, item["label"]]
 
 def build_dataset():
     if configs.data_provider.dataset == 'image_folder':
@@ -21,6 +61,14 @@ def build_dataset():
                                                   transform=ImageTransform()['train'], download=True),
             'val': torchvision.datasets.CIFAR10(configs.data_provider.root, train=False,
                                                 transform=ImageTransform()['val'], download=True),
+        }
+    # modifications for CIFAR10-C
+    elif configs.data_provider.dataset.startswith('cifar10_corrupted'):
+        dataset = {
+            'train': CIFAR10CorruptDataset(configs.data_provider.dataset, split="train", 
+                                                    transform=ImageTransform()["train"]),
+            'val': CIFAR10CorruptDataset(configs.data_provider.dataset, split="val", 
+                                                    transform=ImageTransform()["val"]),
         }
     elif configs.data_provider.dataset == 'cifar100':
         dataset = {
